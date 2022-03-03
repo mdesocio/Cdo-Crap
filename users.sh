85cf4c8349f5477716ac06174ee901f3ac98183c
Michael DeSocio <85764894+mdesocio@users.noreply.github.com>
2022-03-02 22:09:23 -0500

    Create users.sh

---
+++ users.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+sudo deluser blackteam
+sudo deluser earth
+sudo deluser pluto
+sudo deluser neptune
+sudo deluser saturn
+sudo delluser sun
+
+sudo adduser gemini
+sudo usermod -aG sudo gemini
+sudo adduser artemis
+sudo usermod -aG sudo artemis
+sudo adduser mercury
+sudo usermod -aG sudo mercury
+sudo adduser atlantis
+sudo usermod -aG sudo atlantis
+sudo adduser discovery
+sudo usermod -aG sudo discovery
+sudo adduser curiosity
+sudo usermod -aG sudo curiosity
+sudo adduser scoring
+sudo usermod -aG root scoring
