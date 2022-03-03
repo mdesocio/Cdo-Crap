566fa89ffb6e203274439c4039cbfa6668e4003f
Michael DeSocio <85764894+mdesocio@users.noreply.github.com>
2022-03-02 22:22:32 -0500

    Update users.sh

--- users.sh
+++ users.sh
@@ -8,16 +8,23 @@ sudo deluser saturn
 sudo delluser sun
 
 sudo adduser gemini
+echo -e "armstrong#Fa034\narmstrong#Fa034\n" 
 sudo usermod -aG sudo gemini
 sudo adduser artemis
+echo -e "armstrong#Fa034\narmstrong#Fa034\n" 
 sudo usermod -aG sudo artemis
 sudo adduser mercury
+echo -e "armstrong#Fa034\narmstrong#Fa034\n" 
 sudo usermod -aG sudo mercury
 sudo adduser atlantis
+echo -e "armstrong#Fa034\narmstrong#Fa034\n" 
 sudo usermod -aG sudo atlantis
 sudo adduser discovery
+echo -e "armstrong#Fa034\narmstrong#Fa034\n" 
 sudo usermod -aG sudo discovery
 sudo adduser curiosity
+echo -e "armstrong#Fa034\narmstrong#Fa034\n" 
 sudo usermod -aG sudo curiosity
 sudo adduser scoring
+echo -e "armstrong#Fa034\narmstrong#Fa034\n" 
 sudo usermod -aG root scoring
